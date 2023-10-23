0a2a533d376e013b74dcc181154e11fdbf03b0f1
huh354 <143356635+huh354@users.noreply.github.com>
2023-10-22 22:24:01 -0400

    initial upload

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -3,24 +3,67 @@ import { LitElement, html, css } from 'lit';
 export class PolarisChip extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      name: { type: String },
+      link: { type: String },
+      active: { type: Boolean, reflect: true }
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        column-gap: 20px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        line-height: 18.4px;
+        margin-top: 32px;
+        row-gap: 20px;
+        width: 1090px;
+        margin-left: 52px;
+        margin-right: 52px;
+        margin-bottom:0px;
+      }
+      
+      //.tile:focus,
+      //.tile:hover,
+      //:host([active]) .tile {
+      //  background-color: #e4e5e7;
+      //  border: 2px solid #e4e5e7;
+      //  border-radius: 2px;
+      //  color: #005fa9;
+      //  cursor: pointer;
+      //  text-decoration: underline;
+
+      //}
+
+
+      .wrapper{
+        display: grid;
+      }
+
+      .tile {
+        font-weight: bold;
+        text-decoration: none;
+        height: 298.5px;
+        line-height: 18.4;
+        width: 298.5px;
+        padding: 8px 4px;
+        color: rgb(255, 255, 255);
+        background-color: rgb(30, 64, 124);
+        font-size: 16px;
       }
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My boilerplate';
+    this.name = '';
+    this.tile = "https://www.psu.edu/news/research/";
+
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`<a class="tile" href="${this.tile}"><slot>${this.name}</slot></a>`;
   }
 }
